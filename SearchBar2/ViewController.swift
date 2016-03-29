01f14a32c17dd759e7726603d36125c6e11cda30
Ricardo Michel Reyes <me@richiereyes.com>
2016-03-29 16:20:00 -0600

    Initial Commit

---
+++ SearchBar2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchBar2
+//
+//  Created by Ricardo Michel Reyes Martínez on 3/29/16.
+//  Copyright © 2016 Ricardo Michel Reyes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
